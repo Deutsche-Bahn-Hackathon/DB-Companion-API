fcc9d05659c204257e7b9c7601da023b375a1206
Moriz Martiner <martinermoriz@gmail.com>
2016-12-15 14:15:50 +0100

    Basic Swagger configuration

---
+++ swagger.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Moriz
+ * Date: 12/15/2016
+ * Time: 2:10 PM
+ */
+
+/**
+ * @SWG\Swagger(
+ *     schemes={"https"},
+ *     host="deutsche-bahn-api@appspot.gserviceaccount.com",
+ *     basePath="/v1",
+ *     @SWG\Info(
+ *         version="0.0.1",
+ *         title="Swagger Deutsche Bahn",
+ *         description="Api for the Deutsche Bahn project at Deutsche Bahn Hackathon Berlin 2016",
+ *         termsOfService="http://helloreverb.com/terms/",
+ *         @SWG\Contact(
+ *             email="shopit.developments@gmail.com"
+ *         ),
+ *         @SWG\License(
+ *             name="Apache 2.0",
+ *             url="http://www.apache.org/licenses/LICENSE-2.0.html"
+ *         )
+ *     ),
+ *     @SWG\ExternalDocumentation(
+ *         description="Find out more about Swagger",
+ *         url="http://swagger.io"
+ *     )
+ * )
+ */
---
+++ tags.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Moriz
+ * Date: 12/15/2016
+ * Time: 2:14 PM
+ */
+
+/**
+ * @SWG\Tag(
+ *   name="pet",
+ *   description="Everything about your Pets",
+ *   @SWG\ExternalDocumentation(
+ *     description="Find out more",
+ *     url="http://swagger.io"
+ *   )
+ * )
+ * @SWG\Tag(
+ *   name="store",
+ *   description="Operations about user"
+ * )
+ * @SWG\Tag(
+ *   name="user",
+ *   description="Access to Petstore orders",
+ *   @SWG\ExternalDocumentation(
+ *     description="Find out more about our store",
+ *     url="http://swagger.io"
+ *   )
+ * )
+ */
