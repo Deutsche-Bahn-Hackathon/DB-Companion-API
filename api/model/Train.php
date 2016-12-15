2030d50409a8d8d501db7ee870db2b7fb2fdaa76
Moriz Martiner <martinermoriz@gmail.com>
2016-12-15 15:47:50 +0100

    Added some models

---
+++ api/model/Train.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Moriz
+ * Date: 12/15/2016
+ * Time: 3:42 PM
+ */
+
+namespace api\model;
+
+use JsonSerializable
+
+
+class Train implements JsonSerializable {
+    private $id;
+
+    /**
+     * Specify data which should be serialized to JSON
+     * @link http://php.net/manual/en/jsonserializable.jsonserialize.php
+     * @return mixed data which can be serialized by <b>json_encode</b>,
+     * which is a value of any type other than a resource.
+     * @since 5.4.0
+     */
+    function jsonSerialize() {
+        // TODO: Implement jsonSerialize() method.
+    }
+}
