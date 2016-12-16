b20ff345eb5454e19dae018d091b47a1f01e84f4
dejdav <dejoridavid@gmail.com>
2016-12-16 23:22:47 +0100

    Improved FCM

--- util/Fcm.php
+++ util/Fcm.php
@@ -3,4 +3,21 @@
 const API_KEY = 'AIzaSyADu1C07_reKfzioYRZ7is4g69_fO2pujc';
 
 class Fcm {
+
+    public static function send($topic, $receiver, $content) {
+        syslog(LOG_DEBUG, file_get_contents('https://fcm.googleapis.com/fcm/send', false, stream_context_create($data = [
+            'http' => [
+                'header' => 'Content-Type: application/json' . "\r\n" .
+                    'Authorization: key=' . API_KEY . "\r\n",
+                'method' => 'POST',
+                'content' => json_encode([
+                    'to' => '/topics/' . $topic,
+                    'data' => [
+                        'receiver' => $receiver,
+                        'content' => $content
+                    ]
+                ])
+            ]
+        ])));
+    }
 }
