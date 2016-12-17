3df5478e0f41a7880385ca6778b666080595956d
dejdav <dejoridavid@gmail.com>
2016-12-17 15:14:20 +0100

    Changed FCM message

--- api/endpoint/Departure.php
+++ api/endpoint/Departure.php
@@ -8,7 +8,6 @@ use Slim\Http\Response;
 use util\Fcm;
 
 class Departure {
-
     private $containerInterface;
 
     public function __construct(ContainerInterface $containerInterface) {
@@ -16,9 +15,25 @@ class Departure {
     }
 
     public static function update(Request $request, Response $response, array $args) {
-        Fcm::send('ice1206', 'departure', [
-            'departure' => date(DATE_RFC3339, time() + 60),
-            'platform' => '13'
+        Fcm::send('ice1206', 'notification', [
+            'id' => $_POST['id'],
+            'audience' => 'all',
+            'package' => 'com.dbhackathon',
+            'issuedAt' => time(),
+            'expiry' => time() + 3600 ,
+            'titleIt' => 'Train delayed by 10 mins',
+            'titleDe' => 'Train delayed by 10 mins',
+            'messageIt' => 'Your train will depart on platform 3',
+            'messageDe' => 'Your train will depart on platform 3',
+            'minVersion' => 0,
+            'maxVersion' => 0,
+            'color' => 'f44336',
+            'dialogTitleIt' => 'Train delayed by 10 mins',
+            'dialogTitleDe' => 'Train delayed by 10 mins',
+            'dialogTextIt' => 'Your train will depart on platform 3',
+            'dialogTextDe' => 'Your train will depart on platform 3',
+            'dialogNoIt' => 'OK',
+            'dialogNoDe' => 'OK'
         ]);
     }
 }
