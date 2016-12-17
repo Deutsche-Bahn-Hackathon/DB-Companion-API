e580793db6c59b89cc0b4ec7e3d46c5d5823ee4e
dejdav <dejoridavid@gmail.com>
2016-12-17 07:37:26 +0100

    Improvements

--- api/endpoint/Facility.php
+++ api/endpoint/Facility.php
@@ -5,6 +5,7 @@ namespace api\endpoint;
 use Interop\Container\ContainerInterface;
 use Slim\Http\Request;
 use Slim\Http\Response;
+use util\Datastore;
 
 class Facility {
 
@@ -26,26 +27,26 @@ class Facility {
             case 'toilet':
                 $wagon = $args['wagon'];
 
-                if ($wagon >= 23) {
-                    $json = [
-                        'driving_direction' => $wagon == 23 ? null : true,
-                        'to_go' => $wagon - 23,
-                        'wagon' => 23
-                    ];
-                } else if ($wagon == 22) {
-                    $json = [
-                        'driving_direction' => false,
-                        'to_go' => 1,
-                        'wagon' => 23
-                    ];
-                } else {
-                    $json = [
-                        'driving_direction' => $wagon == 20 ? null : true,
-                        'to_go' => $wagon == 20 ? 0 : 1,
-                        'wagon' => 20
-                    ];
+                $toilet = null;
+
+                $toilet_20 = Datastore::toilet()->fetchOne("SELECT * FROM Toilet WHERE train = '${args['train']}' AND wagon = 20");
+                $toilet_23 = Datastore::toilet()->fetchOne("SELECT * FROM Toilet WHERE train = '${args['train']}' AND wagon = 23");
+
+                if ($toilet_20->free && $toilet_23->free) {
+                    $toilet = $args['wagon'] <= 21 ? 20 : 23;
+                } else if ($toilet_20->free) {
+                    $toilet = 20;
+                } else if ($toilet_23->free) {
+                    $toilet = 23;
                 }
 
+                return $response
+                    ->withHeader('Content-Type', 'application/json')
+                    ->withJson($json = [
+                        'driving_direction' => $wagon == 22 ? false : $wagon == 20 || $wagon == 23 ? null : true,
+                        'to_go' => $toilet == null ? null : abs($wagon - $toilet),
+                        'wagon' => $toilet
+                    ]);
                 break;
             default:
                 $json = ['error' => 'Facility must be one of: restaurant; toilet.'];
