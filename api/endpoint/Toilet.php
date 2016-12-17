bd3a9b2be15cd6a7fa622f9d9d3afe5b24fb759e
dejdav <dejoridavid@gmail.com>
2016-12-17 07:37:47 +0100

    Improvements

--- api/endpoint/Toilet.php
+++ api/endpoint/Toilet.php
@@ -15,27 +15,6 @@ class Toilet {
         $this->containerInterface = $containerInterface;
     }
 
-    public static function get(Request $request, Response $response, array $args) {
-        $toilet = null;
-
-        $toilet_20 = Datastore::toilet()->fetchOne("SELECT * FROM Toilet WHERE train = '${args['train']}' AND wagon = 20");
-        $toilet_23 = Datastore::toilet()->fetchOne("SELECT * FROM Toilet WHERE train = '${args['train']}' AND wagon = 23");
-
-        if ($toilet_20->free && $toilet_23->free) {
-            $toilet = $args['wagon'] <= 21 ? 20 : 23;
-        } else if ($toilet_20->free) {
-            $toilet = 20;
-        } else if ($toilet_23->free) {
-            $toilet = 23;
-        }
-
-        return $response
-            ->withHeader('Content-Type', 'application/json')
-            ->withJson([
-                'free' => $toilet
-            ]);
-    }
-
     public static function put(Request $request, Response $response, array $args) {
         $db = Datastore::toilet();
         $toilet = $db->fetchOne("SELECT * FROM Toilet WHERE train = '${args['train']}' AND wagon = ${args['wagon']}");
