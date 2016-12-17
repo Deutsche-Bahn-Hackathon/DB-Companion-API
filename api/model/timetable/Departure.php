9ba867a17a1a939c7548da732062db4fe9ca2405
Moriz Martiner <martinermoriz@gmail.com>
2016-12-17 01:17:19 +0100

    Improved departures

--- api/model/timetable/Departure.php
+++ api/model/timetable/Departure.php
@@ -17,15 +17,65 @@ use JsonSerializable;
  */
 class Departure implements JsonSerializable {
 
+    /**
+     * @SWG\Property(format="string")
+     * @var string
+     */
     private $name;
+
+    /**
+     * @SWG\Property(format="string")
+     * @var string
+     */
     private $type;
+
+    /**
+     * @SWG\Property(format="int32")
+     * @var int
+     */
     private $stop_id;
+
+    /**
+     * @SWG\Property(format="string")
+     * @var string
+     */
     private $stop;
+
+    /**
+     * @SWG\Property(format="dateTime")
+     * @var \DateTime
+     */
     private $datetime;
+
+    /**
+     * @SWG\Property(format="string")
+     * @var string
+     */
     private $direction;
+
+    /**
+     * @SWG\Property(format="string")
+     * @var string
+     */
     private $track;
+
+    /**
+     * @SWG\Property(format="string")
+     * @var string
+     */
+    private $journey;
 
 
     function jsonSerialize() {
+        return [
+            'name' => $this->name,
+            'type' => $this->type,
+            'stop_id' => $this->stop_id,
+            'stop' => $this->stop,
+            'datetime' => $this->datetime,
+            'direction' => $this->direction,
+            'track' => $this->track,
+            'journey' => $this->journey
+        ];
     }
 }
