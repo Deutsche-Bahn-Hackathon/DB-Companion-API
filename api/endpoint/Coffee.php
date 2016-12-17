6d70738ddc29068195dea689dba26039cbca9a64
dejdav <dejoridavid@gmail.com>
2016-12-17 01:53:36 +0100

    Added missing container interface

--- api/endpoint/Coffee.php
+++ api/endpoint/Coffee.php
@@ -3,10 +3,17 @@
 namespace api\endpoint;
 
 use google\appengine\ext\remote_api\Request;
+use Interop\Container\ContainerInterface;
 use Slim\Http\Response;
 use util\Fcm;
 
 class Coffee {
+
+    private $containerInterface;
+
+    public function __construct(ContainerInterface $containerInterface) {
+        $this->containerInterface = $containerInterface;
+    }
 
     public static function startOffer(Request $request, Response $response, array $args) {
         Fcm::send($args['train'], 'coffee', [
